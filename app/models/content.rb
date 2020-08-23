9fe92f2299116c8ad9c5430025fa4821ee57bb49
Daniela Sinclair <dasfluid@gmail.com>
2020-08-23 05:03:59 +0100

    Added validations

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,5 +1,10 @@
 class Content < ApplicationRecord
   belongs_to :user
   has_many :comments
+  has_many :contents_categories
   has_many :categories, through: :contents_categories
+  validates :title, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :categories, presence: true
+  validates :content_type, presence: true, inclusion: { in: [ "Video", "Blog post", "Step-by-Step" ] }
 end
