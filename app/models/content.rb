c757b4b73e683a4dfd9fca60f890a950a9b2e92a
Daniela Sinclair <dasfluid@gmail.com>
2020-08-24 08:57:18 +0100

    Refined JS and added to seed

--- app/models/content.rb
+++ app/models/content.rb
@@ -7,7 +7,6 @@ class Content < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   validates :description, presence: true
   validates :category, presence: true, inclusion: {in: ["Natural", "Artificial", "Chemical Treatment", "Protection"] }
-  validates :style, presence: true, inclusion: {in: ["Plaits", "Bantu knots", "Dreadlocks", "Afro", "Weaves", "Extensions", "Faux locks", "Perms", "Relaxed", "S-curl", "Jerri-curl", "Headscarves and Headwraps"] }
   validates :content_type, presence: true, inclusion: { in: [ "Video", "Blog post", "Step-by-Step" ] }
   validates :video_url, format: { with: url }
   
--- db/seeds.rb
+++ db/seeds.rb
@@ -63,3 +63,46 @@ bantu_tutorial_1 = Content.new(
 bantu_tutorial_1.save
 
 # ContentCategory.new
+
+braids_blog = Content.new(
+    user_id: rand(1..4),
+    title: "How to Wash Box Braids",
+    description: "Lorem ipsum",
+    content_type: "Blog post",
+    category: "Natural",
+    style: "Braids",
+    text: "Box braids give us the freedom to go up to a month or two without the worry of styling, but that doesn’t mean all care goes out of the window! Although you are wearing box braids, you still need to cleanse your hair – especially if you will be wearing them for the long term. This ensures that your scalp won’t become overloaded with buildup, gets itchy or becomes irritated. It also gives your hair much needed contact with good old H20. Here is how you can wash your hair with box braids if you are unsure of how this works:"
+)
+braids_blog.save
+
+growth_blog = Content.new(
+    user_id: rand(1..4),
+    title: "10 Ways To Grow Long Hair Without Protective Styles"
+    description: "Lorem ipsum"
+    content_type: "Blog post"
+    category: "Natural"
+    text: "By now I am sure you have heard a thousand times over about the benefits of protective styling. Sometimes you just want your hair to be free!
+
+    Yes, protective styles are definitely effective, but you can retain hair length with a low manipulation regimen, too.
+
+    Below you will find 10 Steps for Building a Low Manipulation Hair Routine."
+)
+growth_blog.save
+
+half_up_blog = Content.new(
+    user_id: rand(1..4),
+    title: "4 Half Up Half Down Hairstyles for Natural Hair"
+    description: "Lorem ipsum"
+    content_type: "Blog post"
+    category: "Natural"
+    text: "We all have those days where we need a little variety and versatility added to our favorite natural hairstyle.
+
+    If you frequent my site, Natural Hair Rules.com, you know that my favorite natural hair style is the Twist Out. It’s easy and works for any length of hair.
+
+    I’ve been wearing this natural hairstyle since I first went natural to now where I’ve been natural for over 10 years. You can see pictures here.
+
+    A Two Strand Twists and a Twist Out is the perfect natural hairstyle for work, fun, and everything in between.
+
+    You can rock them in a low chignon or bun for a few days. Then when you’re ready to take them down it’s like you have a totally different hairstyle."
+)
+half_up_blog.save
