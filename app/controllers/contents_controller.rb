5f39172da47f60f4d4f13d0d71e3967a157840ff
Jess Pring <jessicapring@outlook.com>
2020-08-24 17:23:03 +0100

    Populated seed file with set categories

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -3,7 +3,7 @@ class ContentsController < ApplicationController
     @contents = Content.all
   end
 
-  def def new
+  def new
     @content = Content.new
     @category = ["plaits", "bantu knots", "dreadlocks", "afro", "weaves", "extensions", "faux locks", "perms", "relaxed", "s-curl", "jerri-curl", "headscarves and headwraps"]
   end
@@ -11,10 +11,10 @@ class ContentsController < ApplicationController
   def create
     @content = Content.new(content_params)
     @content.user = current_user
-      if @content.save
-        redirect_to contents_path(@user)
-      else
-        render :new
-      end
-  end 
+    if @content.save
+      redirect_to contents_path(@user)
+    else
+      render :new
+    end
+  end
 end
