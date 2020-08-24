d6cb17d7de3be7856e295ec7763a62a9faa7980e
jh <oluashegun@googlemail.com>
2020-08-24 16:41:30 +0100

    adding new create and secrets

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,2 +1,17 @@
 class ContentsController < ApplicationController
+
+  def def new
+    @content = Content.new
+    @category = ["plaits", "bantu knots", "dreadlocks", "afro", "weaves", "extensions", "faux locks", "perms", "relaxed", "s-curl", "jerri-curl", "headscarves and headwraps"]
+  end
+
+  def create
+    @content = Content.new(content_params)
+    @content.user = current_user
+      if @content.save
+        redirect_to contents_path(@user)
+      else
+        render :new
+      end
+  end 
 end
--- app/models/content.rb
+++ app/models/content.rb
@@ -2,4 +2,8 @@ class Content < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :categories, through: :contents_categories
+  validates :name, presence: true
+  has_one_attached :video
+  has_many_attached :images
+  validates :category, presence: true inclusion: {in: ["plaits", "bantu knots", "dreadlocks", "afro", "weaves", "extensions", "faux locks", "perms", "relaxed", "s-curl", "jerri-curl", "headscarves and headwraps"]}
 end
