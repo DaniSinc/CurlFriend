721f5c505be64a5a6cd99cdaf51162e504d7d273
Jess Pring <jessicapring@outlook.com>
2020-08-27 15:59:19 +0100

    Pundit fixed for profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
+  skip_before_action :authenticate_user!, only: :show
+
   def show
-    @user = current_user
+    @user = User.find(params[:id])
+    authorize @user
   end
 end
