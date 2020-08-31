5b8a711d58a09181c9713d2b7cf168338ac4d58f
Jess Pring <jessicapring@outlook.com>
2020-08-31 10:47:46 +0100

    pundit for chatroom

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,5 +1,8 @@
 class ChatroomsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :show ]
+
   def show
     @chatroom = Chatroom.find(params[:id])
+    authorize @chatroom
   end
 end
