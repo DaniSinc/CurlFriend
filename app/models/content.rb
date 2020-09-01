23cca2ec3b6ec6787af731ed4332a6af94ee1d5a
Jess Pring <jessicapring@outlook.com>
2020-09-01 12:16:42 +0100

    CSS cleanup for index cards

--- app/models/content.rb
+++ app/models/content.rb
@@ -2,7 +2,7 @@ class Content < ApplicationRecord
   url = /[-a-zA-Z0-9@:%_\+.~#?&\/\/=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)?/
   CATEGORIES = ["Natural", "Artificial", "Chemical treatment", "Protection"]
   STYLES = [ "Afro", "Bantu knots", "Dreadlocks", "Extensions",  "Faux locks", "Headscarves and Headwraps", "Jerry-curl", "Relaxed", "S-curl","Plaits", "Perms", "Weaves" ]
-  CONTENT_TYPE = [ "Video", "Blog post" ]
+  CONTENT_TYPES = [ "Video", "Blog post" ]
 
   belongs_to :user
   has_many :comments, dependent: :destroy
@@ -14,6 +14,7 @@ class Content < ApplicationRecord
   validates :description, presence: true
   validates :category, presence: true, inclusion: { in: CATEGORIES }
   validates :style, presence: true, inclusion: { in: STYLES }
+  validates :content_type, presence: :true, inclusion: { in: CONTENT_TYPES }
 
   def thumbnail_url
     if video_url.present?
